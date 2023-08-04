0335804508913ff115fb8529b485c23f6e15f872
vishal11177 <115709990+vishal11177@users.noreply.github.com>
2023-08-05 00:47:56 +0530

    Update index.js

--- client1/src/api/index.js
+++ client1/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:5001" });
+const api = axios.create({ baseURL: "https://insta-6a56.onrender.com" });
 
 api.interceptors.request.use((req) => {
 
